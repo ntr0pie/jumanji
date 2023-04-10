10d45cfbd8a3fa22de95203c813394154cfbeeb6
ntr0pie <tripathi.shreeshv@gmail.com>
2023-04-10 19:27:47 +0530

    Update optionsRouter

--- routes/optionsRouter.js
+++ routes/optionsRouter.js
@@ -1,11 +1,8 @@
-const exp = require('constants');
 const express = require('express')
 const path = require('path');
+
+
 const router = express.Router();
-
-// const app = express();
-// app.use(express.static(path.join(__dirname, 'public')));
-
 const options = { root: path.join(__dirname, '..', 'public') };
 
 // Home 
