5d05c09d8f8d70b3082bd8fd125680f9440a1aba
ntr0pie <tripathi.shreeshv@gmail.com>
2023-05-01 22:48:03 +0530

    Add loading animation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,6 +10,9 @@ const gameOptions = [
 ]
 const gameImage = document.getElementById('game-image');
 const selectForm = document.getElementById('game-select');
+const gameWrapper = document.getElementsByClassName('game-wrapper')[0];
+const loadingWrapper = document.getElementsByClassName('loading-wrapper')[0];
+const errorWrapper = document.getElementsByClassName('error-wrapper')[0];
 
 const loremIpsumLong = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ipsam quae adipisci quis illum quibusdam ipsum incidunt enim vel, est mollitia molestias ut velit quo, hic illo, officiis deleniti dolor? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ipsam quae adipisci quis illum quibusdam ipsum incidunt enim vel, est mollitia molestias ut velit quo, hic illo, officiis deleniti dolor?'
 const loremIpsumShort = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
@@ -18,9 +21,7 @@ const loremIpsumURL = 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*
 // Generate event functions
 async function generateEvent(userInput)
 {
-
     try {
-        
         const requestObj = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -31,6 +32,7 @@ async function generateEvent(userInput)
 
         console.log("Client: Requesting GPT to initalize game");
 
+        loadingAnimation(1);
         const response = await fetch(genEveEP, requestObj);
 
         if(!response.ok)
@@ -48,9 +50,11 @@ async function generateEvent(userInput)
         gameOptions[2].textContent = data.output.options.c || data.output.options.C;
         gameOptions[3].textContent = data.output.options.d || data.output.options.D;
         gameImage.src = data.output.image_url;
+        loadingAnimation(0);
 
     } catch (error) {
         console.log(error);
+        loadingAnimation(-1);
     }
 
 }
@@ -69,6 +73,21 @@ selectForm.addEventListener('submit', async (event) => {
     await generateEvent(option);
 });
 
-function toggleLoadingAnimation(){
-    // TODO
+function loadingAnimation(state){
+    if (state == 1){
+        errorWrapper.classList.add('hide');
+        gameWrapper.classList.add('hide');
+        loadingWrapper.classList.remove('hide');
+    }
+    if(state == 0){
+        errorWrapper.classList.add('hide');
+        loadingWrapper.classList.add('hide');
+        gameWrapper.classList.remove('hide');
+        
+    }
+    if(state == -1){
+        gameWrapper.classList.add('hide');
+        loadingWrapper.classList.add('hide');
+        errorWrapper.classList.remove('hide');
+    }
 }
